0a215b7e9e1718e49504d8b507cd35d850acb184
Yolanda Arnaspen <210401069@student.umri.ac.id>
2023-10-09 03:02:04 +0700

    first commit

---
+++ fragment/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.betnFragment1.setOnclickListener { replaceFragment(Fragment_1()) }
+        binding.betnFragment1.setOnclickListener { replaceFragment(Fragment_2()) }
+    }
+    private fun replaceFragment(fragment: Fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(androidx.fragment.R.id.fragment_container_view_tag, fragment)
+        fragmentTransaction.commit()
+    }
+}
